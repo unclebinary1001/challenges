a2f00f9e31d2689483df5b2c459920236f0576bc
Mahlangu Nzunda <nzundamahlangu@gmail.com>
2021-12-16 21:00:11 -0500

    SortArrayByParity solution upload

---
+++ SortArrayByParity/sortArrayByParity.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = 0;
+        int j = nums.size()-1;
+        
+        while(i<j){
+            if (nums[i]%2==0){
+                i++;
+            }
+            else{
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j--;
+            }
+        }
+        return nums;
+        
+    }
+};
